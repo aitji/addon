a7b4ee1d8817781d54d7b3adde0a1eb85c244a5e
aitji <ait.suriya@gmail.com>
2025-10-18 18:57:18 +0700

    recode : anti chat
    Co-Authored-By: PickerTH-12 <131840709+pickerth-12@users.noreply.github.com>

--- 1.21.111/AntiSpam/Addon/scripts/lib.js
+++ 1.21.111/AntiSpam/Addon/scripts/lib.js
@@ -4,44 +4,8 @@ system.beforeEvents.watchdogTerminate.subscribe((d) => (d.cancel = true))
 const instructions = `§cAll Chat §7ติดตั้งแล้ว
 
 §l§7#ไอเท็มที่ต้องใช้ §r§c(จำเป็น*)
-§c-รหัสไอเท็ม   §7: minecraft:paper
-§c-ชื่อไอเท็ม    §7: setting
+§c-รหัสไอเท็ม   §7: minecraft:compass
 §c-แท็กแอดมิน  §7: Admin
-
-§l§7#ข้อมูลเพิ่มเติม§r
-§7สำหรับใส่สีชื่อในเกม "color"
-§7เพื่อเปลี่ยนสีชื่อผู้เล่นใช้คำสั่ง §c/scoreboard players set @s color [สีที่ต้องการ]
-§7-0 §fWhite
-§7-1 §0Black
-§7-2 §eYellow
-§7-3 §gMine Coin Gold
-§7-4 §4Dark Red
-§7-5 §cRed
-§7-6 §6Orange
-§7-7 §5Dark Purple
-§7-8 §dLight Purple
-§7-9 §1Dark Blue
-§7-10 §9Blue
-§7-11 §3Dark Aqua
-§7-12 §bAqua
-§7-13 §2Green
-§7-14 §aLime
-§7-15 §8Dark Gray
-§7-16 §7Gray
-
-§7§l##สีใหม่§r
-§7-17 §hQuartz
-§7-18 §iIron
-§7-19 §jNetherite
-§7-20 §mRedstone
-§7-21 §nCopper
-§7-22 §pGold
-§7-23 §qEmerald
-§7-24 §sDiamond
-§7-25 §tLapis Lazuli
-§7-26 §uAmethyst
-§7-27 §vResin
-§o§7*หากเลขสีไม่ถูกต้อง ตัวเลขจะวนกลับไปนับจาก 0 ใหม่ §c(color % 27)
 
 §7ใช้คำสั่ง §c/tag @s add Admin §7เพื่อเข้าเป็นผู้ดูแล ของแอดออนนี้
 §7ผู้ทำแอดออน §caitji §7(1.21.111)
--- 1.21.111/AntiSpam/Addon/scripts/main.js
+++ 1.21.111/AntiSpam/Addon/scripts/main.js
@@ -1,97 +1,113 @@
-import { ScoreboardIdentityType, world, system } from "@minecraft/server";
-import { ModalFormData } from '@minecraft/server-ui'
-system.beforeEvents.watchdogTerminate.subscribe(data => data.cancel = true)
+import { ScoreboardIdentityType, world, system } from "@minecraft/server"
+import { ModalFormData } from "@minecraft/server-ui"
+import "./lib"
+
 system.run(() => {
-    world.scoreboard.getObjective("chatsettings") ? '' : world.scoreboard.addObjective("chatsettings")
-    world.scoreboard.getObjective("delay") ? '' : world.scoreboard.addObjective("delay")
+    world.scoreboard.getObjective("chatsettings")
+        ? ""
+        : world.scoreboard.addObjective("chatsettings")
+    world.scoreboard.getObjective("delay")
+        ? ""
+        : world.scoreboard.addObjective("delay")
 })
-/** --------------------------------------------- */
+
 function getFakePlayer(objectiveId) {
     return world.scoreboard
         .getObjective(objectiveId)
         .getParticipants()
-        .filter(data => data.type === ScoreboardIdentityType.FakePlayer)
-        .map(data => data.displayName)
+        .filter((data) => data.type === ScoreboardIdentityType.FakePlayer)
+        .map((data) => data.displayName)
 }
-/** --------------------------------------------- */
+
 function getScore(objective, target, useZero = true) {
     try {
         const obj = world.scoreboard.getObjective(objective)
-        if (typeof target == 'string') return obj.getScore(obj.getParticipants().find(v => v.displayName == target)) || 0
+        if (typeof target == "string")
+            return (
+                obj.getScore(
+                    obj.getParticipants().find((v) => v.displayName == target)
+                ) || 0
+            )
         return obj.getScore(target.scoreboardIdentity) || 0
     } catch {
         return useZero ? 0 : NaN
     }
 }
-/** --------------------------------------------- */
-world.beforeEvents.itemUse.subscribe((data) => {
-    const pl = data.source
-    const item = data.itemStack
-    data.cancel = true
 
-    if (pl.hasTag('Admin') && item.typeId === "minecraft:compass") system.run(() => antispamui(pl))
-})
-/** --------------------------------------------- */
-function antispamui(pl) {
-    let setting, text, cps
-    try {
-        setting = getFakePlayer('chatsettings')
-        text = setting.filter(str => str.startsWith('text:')).join('').split(":")[1]
-        cps = getScore('chatsettings', 'cps', true) || 0
-    } catch { }
+world.beforeEvents.itemUse.subscribe(
+    ({ source, itemStack }) =>
+        source.hasTag("Admin") &&
+        itemStack.typeId === "minecraft:compass" &&
+        system.run(() => {
+            let text, cps
+            try {
+                const setting = getFakePlayer("chatsettings")
+                text =
+                    setting
+                        .filter((s) => s.startsWith("text:"))
+                        .join("")
+                        .split(":")[1] || "Hey Hey Slow Down please"
+                cps = getScore("chatsettings", "cps", true) || 0
+            } catch { }
 
-    const form = new ModalFormData()
-    form.title(`[§r §lServer Anti Spam§r ]`)
-    form.slider(`\nPlease slider this for chat delay per seconds\nChat Delay`, 0, 10, { valueStep: 1, defaultValue: Number(cps) || 0, tooltip: "This is the delay for player to send chat message" })
-    form.textField(`Warning message when spam`, `ex: Hey Hey Slow Down please`, { defaultValue: text || "Hey Hey Slow Down please", tooltip: "This message will be sent to player when they spam chat" })
+            const form = new ModalFormData()
+            form.title(`[§r §lServer Anti Spam§r ]`)
+            form.slider(`§c§l»§r§c Subscribe §f@aitji.§r\n\n§e§l» §r§fดีเลย์ของ§eแชทต่อวินาที§r`, 0, 10, { valueStep: 1, defaultValue: Number(cps) || 0, tooltip: "นี่คือดีเลย์ของผู้เล่นในการส่งข้อความในแชท", })
+            form.textField(
+                `§c§l» §r§fข้อความเตือนเมื่อ§cสแปม`,
+                `ex: Hey Hey Slow Down please`,
+                {
+                    defaultValue: text || "Hey Hey Slow Down please",
+                    tooltip: "ข้อความนี้จะถูกส่งไปยังผู้เล่นเมื่อสแปมแชท",
+                }
+            )
 
-    form.show(pl).then(res => {
-        if (res.canceled) return
-        let chatsettings = world.scoreboard.getObjective('chatsettings')
-        if (!chatsettings) {
-            world.scoreboard.addObjective('chatsettings', 'Chat Settings')
-            chatsettings = world.scoreboard.getObjective('chatsettings')
-        }
+            form.show(pl).then(({ canceled, formValues }) => {
+                if (canceled) return
+                const chatsettings =
+                    world.scoreboard.getObjective("chatsettings") ||
+                    world.scoreboard.addObjective("chatsettings", "Chat Settings")
 
-        chatsettings.getParticipants().forEach(participant => chatsettings.removeParticipant(participant))
-        chatsettings.setScore("cps", Number(res.formValues[0]))
-        chatsettings.setScore(`text:${res.formValues[1]}`, 1)
-        pl.sendMessage(`§r§7§l| §r§fChat Per Seconds has set to §c"${res.formValues[0]}"`)
-        pl.sendMessage(`§r§7§l| §r§fThe Message has set to §c"${res.formValues[1]}"`)
-    })
-}
-/** --------------------------------------------- */
+                chatsettings
+                    .getParticipants()
+                    .forEach((p) => chatsettings.removeParticipant(p))
+                chatsettings.setScore("cps", Number(formValues[0]))
+                chatsettings.setScore(`text:${formValues[1]}`, 1)
+                pl.sendMessage(`§fAntiSpam is now §aSaved!§r`)
+            })
+        })
+)
+
+// <aitji> if im not lazy i should use system.currentTick, so no loop need
 system.runInterval(() => {
-    const delay = world.scoreboard.getObjective('delay')
+    const delay = world.scoreboard.getObjective("delay")
     for (const player of world.getPlayers()) {
         const count = delay.getScore(player) || 0
         delay.addScore(player, -1)
         if (count - 1 <= 0) delay.removeParticipant(player)
     }
 }, 20)
-/** --------------------------------------------- */
+
 world.beforeEvents.chatSend.subscribe((data) => {
-    const pl = data.sender
-    const msg = data.message
-    const mindcps = getScore('delay', pl, true) || 0
+    const { sender } = data
+
+    const senderCPS = getScore("delay", sender, true) || 0
     let setting, text, cps
     try {
-        setting = getFakePlayer('chatsettings')
-        text = setting.filter(str => str.startsWith('text:')).join('').split(":")[1]
-        cps = getScore('chatsettings', 'cps', true) || 0
+        setting = getFakePlayer("chatsettings")
+        text = setting
+            .filter((str) => str.startsWith("text:"))
+            .join("")
+            .split(":")[1]
+        cps = getScore("chatsettings", "cps", true) || 0
     } catch (lemmyface) { }
 
-    if (mindcps > 0) {
-        pl.sendMessage(`${text || ``} §7: §c${mindcps || 0}`)
+    if (senderCPS > 0) {
+        sender.sendMessage(`${text === '' ? '' : text + '§7: '}§c${senderCPS || 0}`)
         data.cancel = true
         return
     }
 
-    let delay = world.scoreboard.getObjective('delay')
-    if (!delay) {
-        world.scoreboard.addObjective('delay')
-        delay = world.scoreboard.getObjective('delay')
-    }
-    system.run(() => delay.setScore(pl, cps || 0))
+    const delay = world.scoreboard.getObjective("delay")
+    system.run(() => delay.setScore(sender, cps || 0))
 })
-/** --------------------------------------------- */
