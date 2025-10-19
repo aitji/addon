111bf9c7282a36f4c5392adb9ad9381459b12ff6
aitji <ait.suriya@gmail.com>
2025-10-19 14:30:27 +0700

    update nametag & add aitji lib

--- 1.21.111/NameTag/Addon/scripts/index.js
+++ 1.21.111/NameTag/Addon/scripts/index.js
@@ -1,6 +1,5 @@
 /** ------------------- */
-import { system, world } from "@minecraft/server";
-system.beforeEvents.watchdogTerminate.subscribe(data => data.cancel = true)
 import "./Display/NameTag"
 import "./Display/Chat"
+import "./lib"
 /** ------------------- */
