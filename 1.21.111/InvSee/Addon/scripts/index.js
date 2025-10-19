c3e775419d2509f95cd2ab2063ccbffe30e8d618
aitji <ait.suriya@gmail.com>
2025-10-19 13:53:23 +0700

    recode invsee & add lib

--- 1.21.111/InvSee/Addon/scripts/index.js
+++ 1.21.111/InvSee/Addon/scripts/index.js
@@ -1,23 +1,57 @@
-import { system, world } from "@minecraft/server"
-import { invsee } from "./invsee"
-const prefix = "!"
-system.beforeEvents.watchdogTerminate.subscribe(data => data.cancel = true)
+import { system, world, ItemComponentTypes } from "@minecraft/server"
+import "./lib"
 
 world.beforeEvents.chatSend.subscribe(data => {
-    let pl = data.sender
-    if (pl.hasTag('op')) commandHandler(pl, data)
-})
+    const { sender, message } = data
 
-export function commandHandler(player, message) {
-    if (typeof player !== "object") throw TypeError(`player is type of ${typeof player}. Expected "object"`)
-    if (typeof message !== "object") throw TypeError(`message is type of ${typeof message}. Expected "object"`)
+    if ((
+        !sender.hasTag('Admin') &&
+        !sender.hasTag('op')
+    ) || !message.startsWith("!")) return
 
-    if (!message.message.startsWith(prefix)) return;
-    const args = message.message.slice(prefix.length).split(/ +/)
+    const args = message.slice(1).split(/ +/)
     const cmd = args.shift().toLowerCase().trim()
 
     if (cmd === "invsee") {
-        message.cancel = true
-        invsee(message, args)
+        data.cancel = true
+        invsee(data, args)
     }
+})
+
+function invsee(data, args) {
+    const { sender: player } = data
+    if (!args.length) return player.sendMessage("§7You need to provide whos §finventory §7to view!§r")
+
+    let member
+    for (const pl of world.getPlayers())
+        if (pl.name.toLowerCase().includes(args[0].toLowerCase().replace(/"|\\|@/g, ""))) {
+            member = pl
+            break
+        }
+
+    if (!member) return player.sendMessage("§7Couldn't find that §fplayer.§r")
+
+    const container = member.getComponent('inventory').container
+    if (container.size === container.emptySlotsCount)
+        return player.sendMessage(`§r§f${member.name}'s§7 inventory is empty.§r`)
+
+    let inventory = `§f${member.name}'s §7inventory:§r\n\n`
+
+    for (let i = 0; i < container.size; i++) {
+        const item = container.getItem(i)
+        if (!item) continue
+
+        const name = item.typeId.replace("minecraft:", "").replace(/_/g, " ").toLowerCase().replace(/\b[a-z]/g, l => l.toUpperCase())
+        inventory += `§7Slot §f${i}§7: §f${name}:§r§o${item.nameTag || '§r'}§r §7x${item.amount}§r\n`
+
+        const enchComp = item.getComponent(ItemComponentTypes.Enchantable)
+        if (enchComp?.enchantments) {
+            for (const ench of enchComp.enchantments) {
+                const enchName = ench.type.id.charAt(0).toUpperCase() + ench.type.id.slice(1)
+                inventory += `§7       | §f${enchName}§7:§f${ench.level}§r\n`
+            }
+        }
+    }
+
+    player.sendMessage(`§r§c§l§ฟ*-*-*-*-*-*-*-*-*§r\n${inventory}§r§c§l§ฟ*-*-*-*-*-*-*-*-*§r`)
 }
